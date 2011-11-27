007ccb2e660e448990dca5fb4565acf14b8e7fbf
Sarah Hanson <sarahh.hanson@gmail.com>
2011-11-27 17:53:03 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Yumlicious::Application.config.secret_token = '0be169d5b1518c61eb6c4384ffc5b27c3ca5a426f9505958602f6aae6676eaf5cb19829a686f082a127148f74f42ea5ba49fd1d31e6cd389caed0ba7fdd33551'
