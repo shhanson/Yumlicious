2d6b58b71db2a7ff7dbc70934fe569c44d062d31
Sarah Hanson <sarahh.hanson@gmail.com>
2011-11-27 18:47:24 -0600

    Added title tests

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,11 +1,17 @@
 require 'spec_helper'
 
 describe PagesController do
+  render_views
 
   describe "GET 'home'" do
     it "returns http success" do
       get 'home'
       response.should be_success
+    end
+    
+    it "should have the right title" do
+      get 'home'
+      response.should have_selector("title", :content => "Yumlicious | Home")
     end
   end
 
@@ -14,12 +20,21 @@ describe PagesController do
       get 'about'
       response.should be_success
     end
+    it "should have the right title" do
+      get 'about'
+      response.should have_selector("title", :content => "Yumlicious | About")
+    end
   end
 
   describe "GET 'contact'" do
     it "returns http success" do
       get 'contact'
       response.should be_success
+    end
+    
+    it "should have the right title" do
+      get 'contact'
+      response.should have_selector("title", :content => "Yumlicious | Contact")
     end
   end
 
